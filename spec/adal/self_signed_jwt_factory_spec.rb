420a8700b3c968ab3164d39add3d0eda24b72da3
Adam Michael <adam@ajmichael.net>
2015-07-06 11:07:23 -0700

    ruby-jwt fixed off-by-one error in JWT::decode so leeway can be removed from tests.

--- spec/adal/self_signed_jwt_factory_spec.rb
+++ spec/adal/self_signed_jwt_factory_spec.rb
@@ -5,10 +5,6 @@ require 'jwt'
 include FakeData
 
 describe ADAL::SelfSignedJwtFactory do
-  # Note: There is an off-by-one error in JWT::decode in ruby-jwt. I've
-  # submitted a fix at https://github.com/progrium/ruby-jwt/pull/88.
-  # If that fix is submitted, we can remove the options hases from each of the
-  # decoding method calls in this file.
   describe '#create_and_sign_jwt' do
     before(:each) do
       key = OpenSSL::PKey::RSA.new 2048
@@ -21,24 +17,24 @@ describe ADAL::SelfSignedJwtFactory do
 
     it 'should be decodable with the public key' do
       expect do
-        JWT.decode(@jwt, @cert.public_key, options: { leeway: 1 })
+        JWT.decode(@jwt, @cert.public_key)
       end.to_not raise_error
     end
 
     it 'should contain the correct keys in the payload' do
-      payload, = JWT.decode(@jwt, @cert.public_key, options: { leeway: 1 })
+      payload, = JWT.decode(@jwt, @cert.public_key)
       expect(payload.keys).to contain_exactly(
         'aud', 'iss', 'sub', 'nbf', 'exp', 'jti')
     end
 
     it 'should containt the correct keys in the header' do
-      _, header = JWT.decode(@jwt, @cert.public_key, options: { leeway: 1 })
+      _, header = JWT.decode(@jwt, @cert.public_key)
       expect(header.keys).to contain_exactly('x5t', 'alg', 'typ')
     end
 
     it 'should contain the correct thumbprint from the certificate' do
       thumbprint = OpenSSL::Digest::SHA1.new(@cert.to_der).base64digest
-      _, header = JWT.decode(@jwt, @cert.public_key, options: { leeway: 1 })
+      _, header = JWT.decode(@jwt, @cert.public_key)
       expect(header['x5t']).to eq(thumbprint)
     end
   end

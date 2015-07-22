449b01aa873d9c7086da0f6d2c0a2529eada50a3
Adam Michael <adam@ajmichael.net>
2015-07-22 14:17:58 -0700

    Added copyright to all *.rb files.

--- spec/support/fake_data.rb
+++ spec/support/fake_data.rb
@@ -1,3 +1,20 @@
+#-------------------------------------------------------------------------------
+# # Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+# ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A
+# PARTICULAR PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
+#
+# See the Apache License, Version 2.0 for the specific language
+# governing permissions and limitations under the License.
+#-------------------------------------------------------------------------------
+
 # All of the data that the fake token and authority endpoints support. It is
 # separated into its own module so that it can be used as a mix-in in test
 # classes.
